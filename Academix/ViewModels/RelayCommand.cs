b0506abcb5de7e16e423cf985018480b6edadffc
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-05-11 16:32:01 +0700

    Add files via upload

---
+++ Academix/ViewModels/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+public class RelayCommand : ICommand
+{
+    private readonly Action execute;
+    private readonly Func<bool> canExecute;
+
+    public RelayCommand(Action execute, Func<bool> canExecute = null)
+    {
+        this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        this.canExecute = canExecute;
+    }
+
+    public bool CanExecute(object parameter)
+    {
+        return canExecute == null || canExecute();
+    }
+
+    public void Execute(object parameter)
+    {
+        execute();
+    }
+
+    public event EventHandler CanExecuteChanged
+    {
+        add { CommandManager.RequerySuggested += value; }
+        remove { CommandManager.RequerySuggested -= value; }
+    }
+}

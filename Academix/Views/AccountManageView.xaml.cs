2e137f8a0f15d3179991b830a4ab501fd216361f
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-06-19 00:41:05 +0700

    Update AccountManageView.xaml.cs

--- Academix/Views/AccountManageView.xaml.cs
+++ Academix/Views/AccountManageView.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Academix.Views
@@ -17,7 +18,7 @@ namespace Academix.Views
     /// <summary>
     /// Interaction logic for AccountManageView.xaml
     /// </summary>
-    public partial class AccountManageView : Window
+    public partial class AccountManageView : UserControl
     {
         public AccountManageView()
         {

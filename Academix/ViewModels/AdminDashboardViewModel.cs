7ae427db928b77e546954e6d21edcaddbd1722be
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-06-19 00:38:16 +0700

    Add files via upload

---
+++ Academix/ViewModels/AdminDashboardViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Academix.ViewModels
+{
+    public class AdminDashboardViewModel : ObservableObject
+    {
+
+        public bool IsAdmin => LoginWindowViewModel.CurrentUserGroup == "admin";
+
+
+        private UserControl _currentView;
+        public UserControl CurrentView
+        {
+            get => _currentView;
+            set => SetProperty(ref _currentView, value);
+        }
+
+        public IRelayCommand ShowAccountManageViewCommand { get; }
+        public IRelayCommand ShowPermissionManageViewCommand { get; }
+        public IRelayCommand ShowPermissionMatrixViewCommand { get; }
+        public IRelayCommand LogoutCommand { get; }
+
+        public AdminDashboardViewModel()
+        {
+            ShowAccountManageViewCommand = new RelayCommand(() =>
+                CurrentView = new Academix.Views.AccountManageView());
+
+            ShowPermissionManageViewCommand = new RelayCommand(() =>
+                CurrentView = new Academix.Views.PermissionManageView());
+
+            ShowPermissionMatrixViewCommand = new RelayCommand(() =>
+                CurrentView = new Academix.Views.PermissionMatrixView());
+
+            LogoutCommand = new RelayCommand(ExecuteLogout);
+
+            CurrentView = new Academix.Views.AccountManageView();
+        }
+
+        private void ExecuteLogout()
+        {
+            var login = new Views.LoginWindowView();
+            login.Show();
+
+            System.Windows.Application.Current.Windows
+                .OfType<Window>()
+                .FirstOrDefault(w => w.IsActive && w is Views.AdminDashboardView)
+                ?.Close();
+        }
+
+
+
+    }
+
+    
+}

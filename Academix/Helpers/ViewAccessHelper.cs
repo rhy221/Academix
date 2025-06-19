b55baec6ad482097a3928ae9301963cc401cebe1
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-06-20 00:29:05 +0700

    Add files via upload

---
+++ Academix/Helpers/ViewAccessHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Academix.Helpers
+{
+    public static class ViewAccessHelper
+    {
+        public static bool HasAccess(string viewName)
+        {
+            return Session.ManHinhDuocLoadDuocPhep.Contains(viewName);
+        }
+
+        public static UserControl? CreateViewInstance(string viewName)
+        {
+            var fullName = $"Academix.Views.{viewName}";
+            var viewType = Type.GetType(fullName);
+            if (viewType != null && typeof(UserControl).IsAssignableFrom(viewType))
+                return Activator.CreateInstance(viewType) as UserControl;
+            return null;
+        }
+    }
+}

a1ff9b7ed05dd2b87b7a8df8c691c15c12b1951d
vangthanhhuy125 <165678055+vangthanhhuy125@users.noreply.github.com>
2025-06-19 19:27:24 +0700

    Update NguoiDung.cs

--- Academix/Models/NguoiDung.cs
+++ Academix/Models/NguoiDung.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Academix.Models
 {
+    [Table("NGUOIDUNG")]
     public class NguoiDung
     {
         public string TenDangNhap { get; set; }
